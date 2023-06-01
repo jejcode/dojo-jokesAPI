c364e1bbca3a07e3b9dda3b0658ae41b11d2b354
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-06-01 16:48:47 -0600

    added validation

--- models/joke.model.js
+++ models/joke.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const JokeSchema = new mongoose.Schema(
     {
         setup: {
-            type: String, required: true
+            type: String, required: true, minlength: [2, 'Not enough characters']
         },
         punchline: {
             type: String, required: true
